9e98baccd4a5a97fba4eb82623accdae9f66a434
Ali Gohar <aligoharkp@gmail.com>
2024-08-11 20:45:23 +0500

    protected route updated

--- app/components/ProtectedRoute.tsx
+++ app/components/ProtectedRoute.tsx
@@ -1,6 +1,15 @@
 import { useRouter } from "next/navigation"
-const ProtectedRoute = ({children, user}) => {
+import { ReactNode } from "react"
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+  user: boolean; 
+}
+
+const ProtectedRoute = ({ children, user }: ProtectedRouteProps) => {
   const router = useRouter()
+
   return user ? children : router.push('/')
 }
+
 export default ProtectedRoute
